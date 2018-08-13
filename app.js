9059a80339605215fd14316cb55ba55a3861e17e
vangoghjoe <myaddummy@hotmail.com>
2018-08-12 18:19:04 -0700

    Add bootstrap and popper; reorg folder structure

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser'); 
 var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
@@ -10,8 +10,8 @@ var usersRouter = require('./app_server/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'app_server/views'));
+app.set('view engine', 'jade'); 
 
 app.use(logger('dev'));
 app.use(express.json());
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
+var ctrlMain = require('controllers/main');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quick' });
-});
+router.get('/', ctrlMain.index);
 
 module.exports = router;
